961e1c3e81faf4f7b42d99d14d1ff5b10d15d468
Francois Lampasona <lampasonafrancois@gmail.com>
2024-12-24 19:54:36 +0100

    JavaScript

---
+++ JavaScript/Esercizi JS Base/26_Symbol.js
@@ -0,0 +1,16 @@
+/*Esercizio 1: Iteratore Semplice
+Creare un iteratore semplice che iteri su un array di numeri.*/
+/*Esercizio 2: Iteratore Personalizzato
+Creare un iteratore personalizzato per un oggetto.*/
+/*Esercizio 3: Iterare su una Stringa
+Utilizzare un iteratore per iterare su ogni carattere di una stringa.*/
+/*Esercizio 4: Iteratore di un Oggetto con Chiavi e Valori
+Creare un iteratore per un oggetto che restituisca chiavi e valori.*/
+/*Esercizio 5: Iteratore per una Collezione di Oggetti
+Creare un iteratore per una collezione di oggetti che restituisca uno alla volta.*/
+/*Esercizio 6: Iteratore Infinito
+Creare un iteratore infinito che generi numeri interi consecutivi.*/
+/*Esercizio 7: Iteratore di Fibonacci
+Creare un iteratore che generi la sequenza di Fibonacci.*/
+/*Esercizio 8: Iteratore Bidirezionale
+Creare un iteratore bidirezionale che possa iterare avanti e indietro su un array.*/
