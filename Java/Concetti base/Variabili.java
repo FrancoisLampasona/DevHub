d2803bf7f511bb0e630d4991e474a9be44200f3d
Francois Lampasona <lampasonafrancois@gmail.com>
2025-02-16 00:22:23 +0100

    Java First Commit

---
+++ Java/Concetti base/Variabili.java
@@ -0,0 +1,64 @@
+public class Variabili {
+    public static int numero = 10; // Dichiarazione di una variabile statica di tipo intero
+
+    public static void main(String[] args) {
+        System.out.println("Il valore della variabile numero è: " + numero);
+    }
+}
+
+/*
+ * Esercizio 1
+ * Scrivi un programma Java che dichiara una variabile intera chiamata "numero"
+ * e le assegna il valore 10. Stampa il valore della variabile.
+ */
+
+ public static esercizio_1
+
+/*
+ * Esercizio 2
+ * Scrivi un programma Java che dichiara una variabile di tipo float chiamata
+ * "altezza" e le assegna il valore 1.75. Stampa il valore della variabile.
+ */
+
+/*
+ * Esercizio 3
+ * Scrivi un programma Java che dichiara una variabile di tipo boolean chiamata
+ * "isPieno" e le assegna il valore true. Stampa il valore della variabile.
+ */
+
+/*
+ * Esercizio 4
+ * Scrivi un programma Java che dichiara una variabile di tipo char chiamata
+ * "lettera" e le assegna il valore 'A'. Stampa il valore della variabile.
+ */
+
+/*
+ * Esercizio 5
+ * Scrivi un programma Java che dichiara una variabile di tipo String chiamata
+ * "nome" e le assegna il valore "Mario Rossi". Stampa il valore della
+ * variabile.
+ */
+/*
+ * Esercizio 6
+ * Scrivi un programma Java che dichiara una variabile di tipo double chiamata
+ * "peso" e le assegna il valore 68.5. Stampa il valore della variabile.
+ */
+/*
+ * Esercizio 7
+ * Scrivi un programma Java che dichiara due variabili intere chiamate "numero1"
+ * e "numero2" e le assegna i valori 5 e 10 rispettivamente. Calcola la somma
+ * delle due variabili e stampa il risultato.
+ */
+/*
+ * Esercizio 8
+ * Scrivi un programma Java che dichiara due variabili di tipo double chiamate
+ * "prezzo1" e "prezzo2" e le assegna i valori 4.99 e 2.5 rispettivamente.
+ * Calcola la differenza tra le due variabili e stampa il risultato.
+ */
+/*
+ * Esercizio 9
+ * Scrivi un programma Java che dichiara una variabile intera chiamata
+ * "annoDiNascita" e le assegna il valore 1990. Calcola l'età basandoti
+ * sull'anno corrente (supponi che sia il 2023) e stampa il risultato.
+ */
+
