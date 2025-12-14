9dca30728000d57130136b93609f4eb698511bba
Francois Lampasona <lampasonafrancois@gmail.com>
2025-12-14 22:38:44 +0100

    JavaScript

--- JavaScript/Esercizi JS Base/14_Ciclo_While_e_Do_While.js
+++ JavaScript/Esercizi JS Base/14_Ciclo_While_e_Do_While.js
@@ -1,16 +1,40 @@
 /*Esercizio 1
 Scrivi un programma che stampi i numeri da 1 a 5 utilizzando il ciclo while.*/
 
+let numero = 1;
+
+while (numero <= 5) {
+  console.log(numero);
+  numero++;
+}
 
 /*Esercizio 2
 Scrivi un programma che calcoli la somma dei numeri da 1 a 10 utilizzando il ciclo while.*/
 
+let somma = 0;
 
+while (numero <= 10) {
+  somma += numero;
+  numero++;
+}
+
+console.log("La somma è:", somma);
 
 /*Esercizio 3
 Scrivi un programma che stampi i numeri pari da 2 a 10 utilizzando il ciclo do while.*/
+
+numero = 2;
+
+while (numero<=10){
+    console.log(numero % 2 == 0 ? numero : "\t");
+    numero++;
+}
+
 /*Esercizio 4
 Scrivi un programma che prenda un numero in input e conti il numero di cifre presenti utilizzando il ciclo while.*/
+
+
+
 /*Esercizio 5
 Scrivi un programma che generi e stampi la sequenza di Fibonacci fino al numero 100 utilizzando il ciclo do while.*/
 /*Esercizio 6
