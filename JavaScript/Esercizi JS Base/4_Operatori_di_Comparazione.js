91278541fdd14e5de8e654015c54ab02a476840e
Francois Lampasona <lampasonafrancois@gmail.com>
2025-12-14 11:01:58 +0100

    JavaScript

--- JavaScript/Esercizi JS Base/4_Operatori_di_Comparazione.js
+++ JavaScript/Esercizi JS Base/4_Operatori_di_Comparazione.js
@@ -1,36 +1,41 @@
 /*Esercizio 1
 Stampa sulla console il risultato del confronto tra due numeri per verificare se il primo numero è maggiore del secondo.*/
 
-
+console.log (2>1);
 
 /*Esercizio 2
 Stampa sulla console il risultato del confronto tra due stringhe per verificare se la prima stringa viene prima della seconda in ordine alfabetico.*/
 
 
+console.log ("ciao">"mia");
+
 /*Esercizio 3
 Stampa sulla console il risultato del confronto tra un numero e una stringa per verificare se sono uguali.*/
 
-
+console.log ("25"==25);
 
 /*Esercizio 4
 Stampa sulla console il risultato del confronto tra un numero e una stringa per verificare se sono uguali sia in valore che in tipo.*/
 
+console.log ("25"===25);
 
 /*Esercizio 5
 Stampa sulla console il risultato del confronto tra un numero e una stringa per verificare se sono diversi.*/
 
-
+console.log (25 != "25");
 
 /*Esercizio 6
 Stampa sulla console il risultato del confronto tra un numero e una stringa per verificare se sono diversi sia in valore che in tipo.*/
 
+console.log (25 !== "25");
 
 /*Esercizio 7
 Stampa sulla console il risultato del confronto tra due numeri per verificare se il primo numero è minore-uguale del secondo.*/
 
-
+console.log (3<=3);
 
 /*Esercizio 8
 Stampa sulla console il risultato del confronto tra due numeri per verificare se il primo numero è diverso dal secondo.*/
 
+console.log (2 != 4)
 
